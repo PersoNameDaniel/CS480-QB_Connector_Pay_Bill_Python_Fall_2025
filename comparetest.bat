b86d86552a6d3ccb683c986fa3940ebade62d466
Daniel Olsson <olssond@pnw.edu>
2025-10-27 13:49:17 -0500

    compare test logs to file, gitignore ignores that file

--- comparetest.bat
+++ comparetest.bat
@@ -1,1 +1,1 @@
-poetry run python -m src.cli --workbook company_data.xlsx --sheet "account debit vendor" --output report.json
+poetry run python -m src.cli --workbook company_data.xlsx --sheet "account debit vendor" --output report.json > output.log
