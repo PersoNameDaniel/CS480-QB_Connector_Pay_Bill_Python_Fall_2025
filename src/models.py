bf71c18f62dac357841dceb1bccae3a0b7a37dbd
XskullOps <saisridhar.v.007@gmail.com>
2025-10-28 14:16:03 -0500

    Updated the excel reader code and models code

--- src/models.py
+++ src/models.py
@@ -14,6 +14,12 @@ class BillPayment:
     bank_account: str
     amount_to_pay: float
 
+    def __str__(self) -> str:
+        return (
+            f"BillPayment(bill={self.bill}, date={self.date}, "
+            f"bank_account={self.bank_account}, amount_to_pay={self.amount_to_pay})"
+        )
+
 
 __all__ = [
     "BillPayment",
